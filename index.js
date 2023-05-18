bc80cdd7541859a8db8f9731088b004497a421fb
ben <bengarber99@gmail.com>
2023-05-18 09:58:33 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    const gameWidth = 400; // Assuming the game field width is 400px
+    const dodgerWidth = 40; // Assuming the dodger width is 40px
+  
+    if (left + dodgerWidth < gameWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
